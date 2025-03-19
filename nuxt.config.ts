accced0aa51b061a5a60952503baf781c4b56c43
Nooku Demoo <demo@nooku.io>
2025-03-19 20:33:51 +0530

    added vueuse lib

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,12 +2,8 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   css: ['~/assets/css/main.css'],
-  vite: { 
-    server: { hmr: { overlay: false } },
-  },
+  vite: { server: { hmr: { overlay: false } } },
   devtools: { enabled: false },
-  modules: ['@nuxt/ui', '@nookuio/nuxt'],
-  future: {
-    compatibilityVersion: 4
-  }
+  modules: ['@nuxt/ui', '@nookuio/nuxt', '@vueuse/nuxt'],
+  future: { compatibilityVersion: 4 },
 });
